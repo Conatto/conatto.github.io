311bdfbf96afd5dc2c7dd44ed495a31f6d3a6725
Conatto <leoconatto@gmail.com>
2023-04-12 19:59:15 -0300

    directory setting

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/pages/index.html");
 })
 app.get("/home", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/pages/index.html");
 })
 app.get("/projetos", (request, response) => {
-    response.sendFile(__dirname + "/projetos.html");
+    response.sendFile(__dirname + "/pages/projetos.html");
 })
 app.get("/*", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/pages/index.html");
 })
 
 app.listen(4004, () => console.log("Servidor rodando na porta 4004"));
