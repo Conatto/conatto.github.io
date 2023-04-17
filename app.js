7074d814fea91ed1c2cdc40a25498feb4ccde552
Conatto <leoconatto@gmail.com>
2023-04-17 15:07:24 -0300

    directory modified

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const app = express();
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/pages/index.html");
+    response.sendFile(__dirname + "/index.html");
 })
 app.get("/home", (request, response) => {
-    response.sendFile(__dirname + "/pages/index.html");
+    response.sendFile(__dirname + "/index.html");
 })
 app.get("/projetos", (request, response) => {
-    response.sendFile(__dirname + "/pages/projetos.html");
+    response.sendFile(__dirname + "/projetos.html");
 })
 app.get("/*", (request, response) => {
-    response.sendFile(__dirname + "/pages/index.html");
+    response.sendFile(__dirname + "/index.html");
 })
 
 app.listen(4004, () => console.log("Servidor rodando na porta 4004"));
