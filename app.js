4b71e902e5258f4b22e9d056646710dfda203f14
Conatto <leoconatto@gmail.com>
2023-04-16 23:06:46 -0300

    contrabarra

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { dir } = require("console");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -6,16 +7,16 @@ const app = express();
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "\\index.html");
 })
 app.get("/home", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "\\index.html");
 })
 app.get("/projetos", (request, response) => {
-    response.sendFile(__dirname + "/projetos.html");
+    response.sendFile(__dirname + "\\projetos.html");
 })
 app.get("/*", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "\\index.html");
 })
 
 app.listen(4004, () => console.log("Servidor rodando na porta 4004"));
