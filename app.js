3f320f7ba51d012809d74ee6a875aeae30f8af1d
Conatto <leoconatto@gmail.com>
2023-04-27 16:03:52 -0300

    route modifications

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/"));
 
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
-})
-app.get("/home", (request, response) => {
     response.sendFile(__dirname + "/index.html");
 })
 app.get("/projetos", (request, response) => {
